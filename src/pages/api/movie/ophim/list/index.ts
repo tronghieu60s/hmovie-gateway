f309a41b41db06945bcbbd72aeb4f020c6b2d9cf
hieu.trantrong <hieu.trantrong@cisbox.com>
2024-06-13 16:31:48 +0700

    test cors localhost

--- src/pages/api/movie/ophim/list/index.ts
+++ src/pages/api/movie/ophim/list/index.ts
@@ -1,48 +1,54 @@
 import { ApiResponse } from "@/core/dto/api-result.dto";
 import type { NextApiRequest, NextApiResponse } from "next";
+import NextCors from "nextjs-cors";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.setHeader('Cache-Control', 'public, s-maxage=3600');
+  await NextCors(req, res, {
+    methods: ["GET"],
+    origin: ["http://localhost:8081", "https://localhost:8081"],
+  });
 
-  const { page = 1 } = req.query;
+  if (req.method === "GET") {
+    try {
+      const { page = 1 } = req.query;
+  
+      const queryParams = new URLSearchParams();
+      queryParams.append("page", `${page}`);
+  
+      const queryString = queryParams.toString();
 
-  const queryParams = new URLSearchParams();
-  queryParams.append("page", `${page}`);
+      const movies = await fetch(
+        `https://ophim1.com/danh-sach/phim-moi-cap-nhat?${queryString}`
+      ).then((res) => res.json());
 
-  const queryString = queryParams.toString();
+      const items = movies.items.map((item: any) => ({
+        id: item._id,
+        name: item.name,
+        slug: item.slug,
+        originName: item.origin_name,
+        thumbUrl: `${movies.pathImage}${item.thumb_url}`,
+        posterUrl: `${movies.pathImage}${item.poster_url}`,
+      }));
 
-  try {
-    const movies = await fetch(
-      `https://ophim1.com/danh-sach/phim-moi-cap-nhat?${queryString}`
-    ).then((res) => res.json());
+      const pagination = {
+        page: movies.pagination.currentPage,
+        limit: movies.pagination.totalItemsPerPage,
+        totalPage: movies.pagination.totalPages,
+        totalItems: movies.pagination.totalItems,
+      };
 
-    const items = movies.items.map((item: any) => ({
-      id: item._id,
-      name: item.name,
-      slug: item.slug,
-      originName: item.origin_name,
-      thumbUrl: `${movies.pathImage}${item.thumb_url}`,
-      posterUrl: `${movies.pathImage}${item.poster_url}`,
-    }));
-
-    const pagination = {
-      page: movies.pagination.currentPage,
-      limit: movies.pagination.totalItemsPerPage,
-      totalPage: movies.pagination.totalPages,
-      totalItems: movies.pagination.totalItems,
-    };
-
-    res.status(200).json(new ApiResponse({ data: { items, pagination } }));
-  } catch (error) {
-    res.status(500).json(
-      new ApiResponse({
-        data: null,
-        message: `${error}`,
-        success: false,
-      })
-    );
+      res.status(200).json(new ApiResponse({ data: { items, pagination } }));
+    } catch (error) {
+      res.status(500).json(
+        new ApiResponse({
+          data: null,
+          message: `${error}`,
+          success: false,
+        })
+      );
+    }
   }
 }
