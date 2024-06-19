df812f9cdad30e1bc924c7b51f79f66c50107dd0
Trong Hieu <tronghieu60s@gmail.com>
2024-06-19 21:33:36 +0700

    update

--- src/pages/api/kkphim/movies/taxonomy/[slug].ts
+++ src/pages/api/kkphim/movies/taxonomy/[slug].ts
@@ -2,66 +2,65 @@ import { ApiResponse } from "@/core/dto/api-result.dto";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const apiUrl = "https://phimapi.com/v1/api/danh-sach";
-const pageSize = 10;
-
-const taxonomyMapping = {
-  "phim-le": "phim-le",
-  "phim-bo": "phim-bo",
-  "hoat-hinh": "hoat-hinh",
-  "tv-shows": "tv-shows",
-};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const {
-    page: _page = 1,
-    limit: _limit = 24,
-    slug,
-    taxonomy: _taxonomy,
-  } = req.query;
+  if (req.method === "GET") {
+    try {
+      const {
+        slug,
+        page: _page = 1,
+        limit: _limit = 24,
+      } = req.query;
 
-  const queryParams = new URLSearchParams();
-  queryParams.append("page", `${_page}`);
+      let limit = Number(_limit);
+      const page = Number(_page);
 
-  const queryString = queryParams.toString();
+      if (limit < 10) {
+        limit = 10;
+      } else if (limit > 50) {
+        limit = 50;
+      }
 
-  try {
-    if (!Object.keys(taxonomyMapping).includes(_taxonomy)) {
-      throw new Error("invalid taxonomy");
+      const queryParams = new URLSearchParams();
+      queryParams.append("page", `${page}`);
+      queryParams.append("limit", `${limit}`);
+
+      const queryString = queryParams.toString();
+
+      const movies = await fetch(`${apiUrl}/${slug}?${queryString}`).then(
+        (res) => res.json()
+      );
+
+      const pathImage = movies.data.APP_DOMAIN_CDN_IMAGE;
+
+      const items = movies.data.items.map((item: any) => ({
+        name: item.name,
+        slug: item.slug,
+        originName: item.origin_name,
+        thumbUrl: `${pathImage}/${item.thumb_url}`,
+        posterUrl: `${pathImage}/${item.poster_url}`,
+        source: "kkphim",
+      }));
+
+      const pagination = {
+        page: movies.data.params.pagination.currentPage,
+        limit: movies.data.params.pagination.totalItemsPerPage,
+        totalPages: movies.data.params.pagination.totalPages,
+        totalItems: movies.data.params.pagination.totalItems,
+      };
+
+      res.status(200).json(new ApiResponse({ data: { items, pagination } }));
+    } catch (error) {
+      res.status(500).json(
+        new ApiResponse({
+          data: null,
+          message: `${error}`,
+          success: false,
+        })
+      );
     }
-
-    const movies = await fetch(`${apiUrl}/${slug}?${queryString}`).then((res) =>
-      res.json()
-    );
-
-    const pathImage = movies.data.APP_DOMAIN_CDN_IMAGE;
-
-    const items = movies.data.items.map((item: any) => ({
-      name: item.name,
-      slug: item.slug,
-      originName: item.origin_name,
-      thumbUrl: `${pathImage}/${item.thumb_url}`,
-      posterUrl: `${pathImage}/${item.poster_url}`,
-      source: "kkphim",
-    }));
-
-    const pagination = {
-      page: movies.paginate.current_page,
-      limit: movies.paginate.items_per_page,
-      totalPages: movies.paginate.total_page,
-      totalItems: movies.paginate.total_items,
-    };
-
-    res.status(200).json(new ApiResponse({ data: { items, pagination } }));
-  } catch (error) {
-    res.status(500).json(
-      new ApiResponse({
-        data: null,
-        message: `${error}`,
-        success: false,
-      })
-    );
   }
 }
--- src/pages/api/phimnguonc/movies/taxonomy/[slug].ts
+++ src/pages/api/phimnguonc/movies/taxonomy/[slug].ts
@@ -1,78 +1,93 @@
 import { ApiResponse } from "@/core/dto/api-result.dto";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-const taxonomyMapping = {
-  nam: "nam-phat-hanh",
-  "the-loai": "the-loai",
-  "dinh-dang": "danh-sach",
-  "quoc-gia": "quoc-gia",
-};
+const apiUrl = "https://phim.nguonc.com/api/films/dinh-dang";
+const pageSize = 10;
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { slug, taxonomy: _taxonomy, page = 1 } = req.query;
-  let taxonomy = _taxonomy as keyof typeof taxonomyMapping;
+  if (req.method === "GET") {
+    try {
+      const { slug, page: _page = 1, limit: _limit = 24 } = req.query;
 
-  const queryParams = new URLSearchParams();
-  queryParams.append("page", `${page}`);
+      let limit = Number(_limit);
+      const page = Number(_page);
 
-  const queryString = queryParams.toString();
+      if (limit < 10) {
+        limit = 10;
+      } else if (limit > 50) {
+        limit = 50;
+      }
 
-  if (!taxonomy) {
-    taxonomy = "dinh-dang";
-  }
+      const movies = [];
 
-  if (!Object.keys(taxonomyMapping).includes(taxonomy)) {
-    throw new Error("invalid taxonomy");
-  }
+      let queryPage = Math.ceil((page * limit) / pageSize);
+      let totalItems = 0;
 
-  const taxonomyValue = taxonomyMapping[taxonomy];
+      do {
+        const queryParams = new URLSearchParams();
+        queryParams.set("page", `${queryPage}`);
 
-  try {
-    const movies = await fetch(
-      `https://phim.nguonc.com/api/films/${taxonomyValue}/${slug}?${queryString}`
-    ).then((res) => res.json());
+        const queryString = queryParams.toString();
 
-    const items = movies.items.map((item: any) => ({
-      name: item.name,
-      slug: item.slug,
-      originName: item.origin_name,
-      thumbUrl: item.thumb_url,
-      posterUrl: item.poster_url,
-      content: item.description,
-      totalEpisodes: item.total_episodes,
-      currentEpisode: item.current_episode,
-      quality: item.quality,
-      duration: item.item,
-      language: item.language,
-      casts: (item.casts || "")
-        .split(",")
-        .map((item: string) => item.trim())
-        .filter((item: string) => item),
-      directors: (item.director || "")
-        ?.split(",")
-        .map((item: string) => item.trim())
-        .filter((item: string) => item),
-      source: "phimnguonc",
-    }));
+        queryPage -= 1;
 
-    const pagination = {
-      page: movies.paginate.current_page,
-      limit: movies.paginate.items_per_page,
-      totalPages: movies.paginate.total_page,
-      totalItems: movies.paginate.total_items,
-    };
+        const response = await fetch(`${apiUrl}/${slug}?${queryString}`).then(
+          (res) => res.json()
+        );
 
-    res.status(200).json(new ApiResponse({ data: { items, pagination } }));
-  } catch (error) {
-    res.status(500).json(
-      new ApiResponse({
-        data: null,
-        message: `${error}`,
-        success: false,
-      })
-    );
+        movies.unshift(...response.items);
+        if (!totalItems) {
+          totalItems = response.paginate.total_items;
+        }
+      } while (movies.length < limit);
+
+      const startIndex = limit * (page - 1) - queryPage * pageSize;
+      const endIndex = startIndex + limit;
+
+      const moviesItems = movies.slice(startIndex, endIndex);
+
+      const items = moviesItems.map((item: any) => ({
+        name: item.name,
+        slug: item.slug,
+        originName: item.origin_name,
+        thumbUrl: item.thumb_url,
+        posterUrl: item.poster_url,
+        content: item.description,
+        totalEpisodes: item.total_episodes,
+        currentEpisode: item.current_episode,
+        quality: item.quality,
+        duration: item.item,
+        language: item.language,
+        casts: (item.casts || "")
+          .split(",")
+          .map((item: string) => item.trim())
+          .filter((item: string) => item),
+        directors: (item.director || "")
+          ?.split(",")
+          .map((item: string) => item.trim())
+          .filter((item: string) => item),
+        source: "phimnguonc",
+      }));
+
+      const pagination = {
+        page,
+        limit,
+        totalPages: Math.ceil(totalItems / limit),
+        totalItems,
+      };
+
+      res.status(200).json(new ApiResponse({ data: { items, pagination } }));
+    } catch (error) {
+      res.status(500).json(
+        new ApiResponse({
+          data: null,
+          message: `${error}`,
+          success: false,
+        })
+      );
+    }
   }
 }
